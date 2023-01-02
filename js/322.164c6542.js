e041e28209478dda3f8741bc271a08b4f30efc46
robinmalburn <robin.malburn@googlemail.com>
2023-01-02 15:12:02 +0000

    Deploying to gh-pages from @ robinmalburn/tyov@064a3711a02957aa4e6ae7ca117079f65cd33910 🚀

--- js/322.164c6542.js
+++ js/322.164c6542.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunktyov"]=self["webpackChunktyov"]||[]).push([[322],{3322:function(e,r,t){t.r(r);var i=t(8204);r["default"]={description:"Adds ID to memories and events.",requiredSignature:1,migrate(e){return e.memories.forEach((e=>{e.id||(e.id=(0,i.Z)("memory")),e.events.forEach(((e,r,t)=>{"string"===typeof e&&(e={description:e,id:(0,i.Z)("event")},t[r]=e)}))})),e}}}}]);
-//# sourceMappingURL=322.dcd08e41.js.map
+//# sourceMappingURL=322.164c6542.js.map
--- js/46.4931df82.js
+++ js/46.4931df82.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunktyov"]=self["webpackChunktyov"]||[]).push([[46],{9046:function(e,i,s){s.r(i);var r=s(8204);i["default"]={description:"Adds ID to skills.",requiredSignature:1,migrate(e){return e.skills.forEach((e=>{e.id||(e.id=(0,r.Z)("skill"))})),e}}}}]);
-//# sourceMappingURL=46.0e0ac4b9.js.map
+//# sourceMappingURL=46.4931df82.js.map
