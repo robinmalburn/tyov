7c3fa3e93ac616c5eba8eba91a75f994858ca7a4
Robin Malburn <robin@malburn.co.uk>
2025-07-26 10:31:17 +0100

    adds basic vitest support

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue2";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
   resolve: {
     alias: {
       Libs: "/src/lib",
