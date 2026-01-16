a3d7ca4e11460c44b24cf0ea6f883914e0f8d657
Robin Malburn <robin@malburn.co.uk>
2026-01-16 13:19:13 -0800

    Fixes base path configuration in vite.

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "./",
   plugins: [vue(), tailwindcss()],
   test: {
     environment: "jsdom",
